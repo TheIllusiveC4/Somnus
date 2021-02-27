bdb6964c2cd6db69591adbcfaff010128140001a
TheIllusiveC4 <itwasmec4@gmail.com>
2021-02-26 22:54:43 -0800

    fix bed block message NPE, closes #1

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -4,5 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) and this project uses
 [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [0.0.11-1.16.5] - 2021.02.26
+### Changed
+- Updated to Minecraft 1.16.5
+### Fixed
+- Fixed vanilla bug with bed blocks sending null messages [#1](https://github.com/TheIllusiveC4/Somnus/issues/1)
+
 ## [0.0.10-1.16.4] - 2021.01.04
 Initial public beta release
---
+++ src/main/java/top/theillusivec4/somnus/mixin/MixinBedBlock.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2021 C4
+ *
+ * This file is part of Somnus, a mod made for Minecraft.
+ *
+ * Somnus is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or any later version.
+ *
+ * Somnus is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with Somnus.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package top.theillusivec4.somnus.mixin;
+
+import net.minecraft.block.BedBlock;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * Fix for vanilla bug where bed block doesn't check for null messages before sending
+ */
+@Mixin(BedBlock.class)
+public class MixinBedBlock {
+
+  @Inject(at = @At(value = "INVOKE", target = "net/minecraft/entity/player/PlayerEntity.sendMessage(Lnet/minecraft/text/Text;Z)V"), method = "method_19283(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/entity/player/PlayerEntity$SleepFailureReason;)V", cancellable = true)
+  private static void _somnus_sendMessage(PlayerEntity unused, PlayerEntity.SleepFailureReason reason, CallbackInfo ci) {
+
+    if (reason.toText() == null) {
+      ci.cancel();
+    }
+  }
+}
